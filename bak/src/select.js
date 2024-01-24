4d712bcdccdaf8417769fdbb5ae41b4b8ccbaa52
axel sandbakken <axel.sandbakken@gmail.com>
2024-01-24 12:39:00 +0100

    7hy76hy6hy67

---
+++ bak/src/select.js
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import './table_style.css';
+
+export default function Select() {
+    const [elevData, setElevData] = useState([]);
+
+    useEffect(() => {
+        getElevData();
+    }, []);
+
+    const getElevData = () => {
+        axios
+            .get("http://localhost:3500/")
+            .then(response => {
+                setElevData(response.data);
+            })
+            .catch(error => console.log(error));
+    };
+
+    return (
+        <>
+        <div className="table-container">
+            
+            <table className="styled-table">
+                <thead>
+                    <tr>
+                        <th>ElevID</th>
+                        <th>Fornavn</th>
+                        <th>Etternavn</th>
+                        <th>DatamaskinID</th>
+                        <th>Hobby</th>
+                        <th>Klasse</th>
+                        <th>Kjønn</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {elevData.map(elev => (
+                        <tr key={elev.ElevID}>
+                            <td>{elev.ElevID}</td>
+                            <td>{elev.Fornavn}</td>
+                            <td>{elev.Etternavn}</td>
+                            <td>{elev.DatamaskinID}</td>
+                            <td>{elev.Hobby}</td>
+                            <td>{elev.Klasse}</td>
+                            <td>{elev.Kjonn}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </>
+    );
+}
